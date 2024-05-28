40be1c32124c4a6a3ea2803ce33c6043b29b8651
DavidOngora <ongoradavid5.gmail.com>
2024-05-28 12:44:13 +0300

    strictly creates one table

--- database/migrations/2024_05_22_143536_create_contacts_table.php
+++ database/migrations/2024_05_22_143536_create_contacts_table.php
@@ -4,28 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateContactsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
-{
-    Schema::create('contacts', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->string('email')->unique();
-        $table->string('phone');
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->unsignedBigInteger('group_id')->nullable();
+            $table->timestamps();
 
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('set null');
+        });
+    }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('contacts');
     }
-};
+}
